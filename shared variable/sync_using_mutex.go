e150a6b1f8e665ad6dec383676b1f1daa1b13d41
Arjit singh <arjitsingh8@gmail.com>
2023-08-21 22:35:48 +0530

    using mutexes

--- shared variable/sync_using_mutex.go
+++ shared variable/sync_using_mutex.go
@@ -11,11 +11,15 @@ type Bank struct {
 	Balance int
 }
 
-func (b Bank) Deposit(amount int) {
+func (b *Bank) Deposit(amount int) {
+	b.mu.Lock()
 	b.Balance = b.Balance + amount
+	b.mu.Unlock()
 }
 
-func (b Bank) balance() int {
+func (b *Bank) balance() int {
+	b.mu.Lock()
+	defer b.mu.Unlock()
 	return b.Balance
 }
 
@@ -25,13 +29,16 @@ func main() {
 	}
 
 	for i := 0; i < 10; i++ {
-		go func(newBank Bank) {
+		go func(newBank *Bank) {
+			// fmt.Println("first")
 			newBank.Deposit(100)
 			fmt.Println(newBank.balance())
-		}(newBank)
-		go func() {
+		}(&newBank)
+
+		go func(newBank *Bank) {
+			// fmt.Println("second")
 			newBank.Deposit(200)
-		}()
+		}(&newBank)
 	}
 
 	time.Sleep(time.Second * 1)
