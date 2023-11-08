df5668b2749d54b00416192642584e63e4461c6f
Arjit singh <arjitsingh8@gmail.com>
2023-11-08 22:05:34 +0530

    implemented mux with server

---
+++ chat server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = ":8080"
+
+func main() {
+	// fmt.Println("Hello, World !")
+
+	// server := http.NewServeMux()
+
+	// server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("hello"))
+	// })
+
+	// err := http.ListenAndServe(port, server)
+
+	server := http.Server{
+		Addr: port,
+	}
+
+	serverMux := http.NewServeMux()
+
+	serverMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hey there<h1/>"))
+	})
+	serverMux.HandleFunc("/new", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hey New<h1/>"))
+	})
+	server.Handler = serverMux
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Running on port : %s", port))
+}
