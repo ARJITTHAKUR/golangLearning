37276dbcaee191e31290a2a9aeb9bb09545bda5b
Arjit singh <arjitsingh8@gmail.com>
2023-11-23 22:55:14 +0530

    learing method functions

--- Methods/main.go
+++ Methods/main.go
@@ -11,10 +11,17 @@ func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+func (p *Point) Scale(val float64) {
+	p.X *= val
+	p.Y *= val
+}
+
 func main() {
 
 	p := Point{1, 2}
 	q := Point{4, 6}
 
 	fmt.Println(p.Distance(q))
+	p.Scale(2.0)
+	fmt.Println(p)
 }
