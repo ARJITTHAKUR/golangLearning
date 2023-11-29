cd2b34aa457864eb4dd06e5a5b02d665d0355887
Arjit singh <arjitsingh8@gmail.com>
2023-11-29 23:15:23 +0530

    golang pointer

---
+++ pointers/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int = 10
+	var ptr *int = &x
+	anotherPtr := &x
+	fmt.Println(x)
+	fmt.Println(ptr)
+	fmt.Println(*ptr)
+	fmt.Println(anotherPtr)
+
+	arr := [3]int{1, 2, 3}
+
+	arrPtr := &arr[0]
+
+	fmt.Println(*arrPtr)
+	// arrPtr++
+	// arrPtr = (*int)(unsafe.Pointer(
+	// 	uintptr(unsafe.Pointer(ptr))
+	// 	 + uintptr(unsafe.Sizeof(arr[0])))
+	// 	 )
+
+	fmt.Println(*arrPtr)
+}
