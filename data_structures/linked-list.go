f758490868745b270d0e9fbc36a39f7434c2f335
Arjit singh <arjitsingh8@gmail.com>
2023-12-03 22:04:36 +0530

    implemented add to the end and last node for linked list

--- data_structures/linked-list.go
+++ data_structures/linked-list.go
@@ -10,7 +10,7 @@ type LinkedList struct {
 	headNode *Node
 }
 
-func (linkedList *LinkedList) Add(val int) {
+func (linkedList *LinkedList) AddToHead(val int) {
 	var node = Node{}
 	node.property = val
 	if node.nextNode != nil {
@@ -19,11 +19,34 @@ func (linkedList *LinkedList) Add(val int) {
 
 	linkedList.headNode = &node
 }
+
+func (linkedList *LinkedList) LastNode() *Node {
+	var node *Node
+	var lastNode *Node
+
+	for node = linkedList.headNode; node != nil; node = node.nextNode {
+		if node.nextNode == nil {
+			lastNode = node
+		}
+	}
+	return lastNode
+}
+
+func (linkedList *LinkedList) AddToEnd(val int) {
+	var newNode = &Node{property: val}
+	var lastNode *Node
+	lastNode = linkedList.LastNode()
+	if lastNode != nil {
+		lastNode.nextNode = newNode
+	}
+}
 func main() {
 
 	var linkedList LinkedList
-	linkedList.Add(1)
-	linkedList.Add(2)
+	linkedList.AddToHead(1)
+	linkedList.AddToHead(2)
+	linkedList.AddToEnd(56)
 
 	fmt.Println(linkedList.headNode.property)
+	fmt.Println("last node value is", linkedList.LastNode())
 }
