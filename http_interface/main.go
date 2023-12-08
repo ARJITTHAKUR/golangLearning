613ebb8bd7a2bf858cccf1c8b879dd860345b456
Arjit singh <arjitsingh8@gmail.com>
2023-12-08 23:46:42 +0530

    extended functionality

--- http_interface/main.go
+++ http_interface/main.go
@@ -15,8 +15,27 @@ func (d dollar) String() string {
 type database map[string]dollar
 
 func (d database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for item, price := range d {
-		fmt.Fprintf(w, "%s\n", item, price)
+	// for item, price := range d {
+	// 	fmt.Fprintf(w, "%s\n", item, price)
+	// }
+
+	switch req.URL.Path {
+	case "/list":
+		for item, price := range d {
+			fmt.Fprintf(w, "%s\n", item, price)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := d[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "no such item : %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such page: %s\n", req.URL)
 	}
 }
 
