af92afe5bfc63be61eb8d332a660a6614ef07056
Arjit singh <arjitsingh8@gmail.com>
2023-12-11 22:19:42 +0530

    made changes

--- proxy_server/server_one.go
+++ proxy_server/server_one.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 
-	fmt.Println("starting server 1")
+	// fmt.Println("starting server 1")
 	http.HandleFunc("/one", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("server one responding")
+		// fmt.Println("server one responding")
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 	log.Fatal(http.ListenAndServe("localhost:8081", nil))
--- proxy_server/server_two.go
+++ proxy_server/server_two.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 
-	fmt.Println("starting server 2")
+	// fmt.Println("starting server 2")
 	http.HandleFunc("/two", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("server two responding")
+		// fmt.Println("server two responding")
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 	log.Fatal(http.ListenAndServe("localhost:8082", nil))
